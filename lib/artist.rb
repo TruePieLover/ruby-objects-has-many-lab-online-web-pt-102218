a22ef70f7f00169e039ffcf75312c57cf67cd284
Brandon Teich <brandonteich123@gmail.com>
2018-11-22 04:40:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,25 @@
-
-  def self.song_count
-    Song.array.count
-    enclass Author
-    attr_accessor :name
+class Artist
+   attr_accessor :name
    @@all = []
- def initialize(name)
+  def initialize(name)
     @name = name
     @@all << self
   end
    def self.all
     @@all
   end
-   def add_post(post)
-    post.author = self
+  def add_song(song)
+    song.artist = self
   end
-   def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
+   def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
   end
-   def posts
-    Post.all.select {|post| post.author == self}
+  def songs
+    Song.all.select do |x| song.artist == self
   end
-   def self.post_count
-    Post.all.count
+  end
+   def self.song_count
+    Song.all.count
   end
 end
