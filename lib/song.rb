cd658367940f032a3c99fd7b43390641243ae073
Brandon Teich <brandonteich123@gmail.com>
2018-11-22 04:05:15 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  attr_accessor :name, :artist
+   @@array = []
+   def initialize(name)
+    @name = name
+    @@array << self
+  end
+   def self.all
+    @@all
+  end
+   def artist_name
+    if self.artist
+      self.artist.name
+    else
+      break
+    end
+  end
+end
