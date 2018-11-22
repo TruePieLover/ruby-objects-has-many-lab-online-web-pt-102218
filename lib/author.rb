1762e564928d1a5733676f89026b97c4bc584c13
Brandon Teich <brandonteich123@gmail.com>
2018-11-22 04:27:39 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,22 +1,21 @@
-
 class Author
-  attr_accessor :name
-   @@all = []
+attr_accessor :name
+   @@array = []
    def initialize(name)
     @name = name
-    @@all << self
+    @@array << self
   end
    def self.all
-    @@all
+    @@array
   end
-   def add_post(post)
+  def add_post(post)
     post.author = self
   end
    def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
   end
-   def posts
+  def posts
     Post.all.select {|post| post.author == self}
   end
    def self.post_count
