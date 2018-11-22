3bc07d6a1964c0db94ae7f6ba0e6f636304b94c8
Brandon Teich <brandonteich123@gmail.com>
2018-11-22 04:28:34 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,11 @@
 class Post
 attr_accessor :title, :author
-   @@array = []
+  @@array = []
   def initialize(title)
     @title = title
     @@array << self
   end
-   def self.all
+   def self.array
     @@array
   end
    def author_name
